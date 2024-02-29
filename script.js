7ecdac95a8113b929f69867ca95957164636c355
marwanmohamedd7 <maro.jk2345@gmail.com>
2024-02-29 03:11:56 +0200

    New verision

--- script.js
+++ script.js
@@ -1,118 +1,270 @@
-'use strict';
-/*
-Both of math.trunc() or toFixed() remove the decimeals
-but math.trunc() return number 
-toFixed() return string
-*/
-// body access
-const gameBody = document.querySelector('body');
-// secret number box access
-const secretNumBox = document.querySelector('.number');
-// again Btn access
-const againBtn = document.querySelector('.again');
-// check Btn access
-const checkBtn = document.querySelector('.check');
-// guess input Box access
-const guessInput = document.querySelector('.guess');
-// generate secretNumber
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
-let score = 20;
-let highScore = 0;
-let ifEmpty = true;
+"use strict";
+let guess = Math.trunc(Math.random() * 20) + 1;
+let gameScore = 0;
+let gameHighScore = 0;
+let gameTries = 30;
+let highScoreBoxValue;
+let checkInputValue = 0;
+const checkBtn = document.querySelector(".checkBtn");
+let agianBtn = document.querySelector(".restart");
+let checkMessage = ``;
+console.log(guess);
 
-// reset the game by pressing again
-// againBtn = document.querySelector('.again')
-againBtn.addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  overwriteMess('.score', score);
-  overwriteMess('.message', 'Start guessing...');
-  overwriteMess('.number', '?');
-  guessInput.value = '';
-  secretNumBox.style.width = '15rem';
-  gameBody.style.backgroundColor = '#222';
-});
+function wrongIcon() {
+  return document.querySelector(".item-messagebox .fa-solid");
+}
+function messageBoxBtn1() {
+  return document.querySelector(".item-messagebox-btns .btn-1");
+}
+function messageBoxBtn2() {
+  return document.querySelector(".item-messagebox-btns .btn-2");
+}
+function messageBox() {
+  return document.querySelector(".messagebox");
+}
+function messageBoxAddDisplay() {
+  return messageBox().classList.add("d-none");
+}
+function messageBoxRemoveDisplay() {
+  return messageBox().classList.remove("d-none");
+}
 
-// when user press on check button
-// checkBtn = document.querySelector('.check')
-checkBtn.addEventListener('click', function () {
-  const guess = Number(guessInput.value);
-  // when guess greater or smaller than range (1-20)
-  if (guess < 1 || guess > 20) {
-    overwriteMess('.message', '❌ Number between 1-20');
+function messageBoxContent(message) {
+  return (document.querySelector(".item-messagebox-message h5").innerHTML =
+    message);
+}
+
+function highScoreSaveBox(message) {
+  document.querySelector(".highScoreValue").innerHTML = message;
+}
+
+function localStorageData() {
+  if (localStorage.getItem("currentHighScore") != null) {
+    highScoreBoxValue = localStorage.getItem("currentHighScore");
+    highScoreSaveBox(highScoreBoxValue);
   } else {
-    // when there is no input
-    if (!guess) {
-      overwriteMess('.message', '⛔ No Number');
-    }
-    // when player wins
-    else if (guess === secretNumber) {
-      overwriteMess('.message', '🎉 Corret Number');
-      // gameBody = document.querySelector('body')
-      gameBody.style.backgroundColor = '#60b347';
-      overwriteMess('.number', secretNumber);
-      // secretNumBox = document.querySelector('.number')
-      secretNumBox.style.width = '30rem';
-      if (score > highScore) {
-        highScore = score;
-        overwriteMess('.highscore', highScore);
+    highScoreBoxValue = 0;
+    highScoreSaveBox(highScoreBoxValue);
+  }
+}
+
+// document.querySelector('.reset').addEventListener('click',()=>clearLocalStorage())
+function clearLocalStorage() {
+  localStorageData(localStorage.clear());
+}
+
+function gameMessage(message) {
+  document.querySelector(".resultChecked").innerHTML = message;
+  return message;
+}
+
+function countGameTries(message) {
+  document.querySelector(".tries").innerHTML = message;
+}
+
+function countGameScore(message) {
+  document.querySelector(".score").innerHTML = message;
+}
+
+function changebackGroundColor(message) {
+  document.querySelector("body").style.cssText = message;
+}
+
+function checkBtnDisable() {
+  checkBtn.classList.add("disabled");
+}
+
+function checkBtnEnable() {
+  checkBtn.classList.remove("disabled");
+}
+
+function countHighScore(message) {
+  document.querySelector(".high_score").innerHTML = message;
+}
+
+function showSecretNumber(message) {
+  document.querySelector(".secretNumber h1").innerHTML = message;
+}
+function secretNumberStyle(message) {
+  document.querySelector(".secretNumber h1").style.cssText = message;
+}
+
+function emptyInput() {
+  document.querySelector(".checkInput").value = ``;
+}
+function checkInput() {
+  return document.querySelector(".checkInput");
+}
+
+function inputDisable(checkInput) {
+  checkInput.classList.add("disabled");
+}
+function inputEnable(checkInput) {
+  checkInput.classList.remove("disabled");
+}
+
+function check(checkInput) {
+  checkInputValue = Number(checkInput.value);
+  if (checkMessage === `&#127881; correct number!`) {
+    messageBoxRemoveDisplay();
+    messageBoxContent(
+      `&#10060; you have to press Agian button at the top of the screen or Esc button on your keyBoard or ok button`
+    );
+  } else {
+    const ResCheckInput = Number(checkInput.value);
+    checkBtnEnable();
+    if (!ResCheckInput && gameTries > 1 && checkInput.value === ``) {
+      gameTries--;
+      console.log(gameTries);
+      countGameTries(gameTries);
+      gameMessage(`&#9940; no guessing number!`);
+    } else if (ResCheckInput === guess) {
+      changebackGroundColor(`background-color:#60b347`);
+      checkMessage = gameMessage(`&#127881; correct number!`);
+      showSecretNumber(guess);
+      secretNumberStyle(`width:12.5rem`);
+      checkBtnDisable();
+      inputDisable(checkInput);
+      if (gameTries <= 30 && gameTries >= 20) {
+        gameScore += 10;
+        gameHighScore += gameScore;
+        countGameScore(gameScore);
+        countHighScore(gameHighScore);
+      } else if (gameTries < 20 && gameTries >= 10) {
+        gameScore += 8;
+        gameHighScore += gameScore;
+        countGameScore(gameScore);
+        countHighScore(gameHighScore);
+      } else if (gameTries < 10 && gameTries >= 1) {
+        gameScore += 6;
+        gameHighScore += gameScore;
+        countGameScore(gameScore);
+        countHighScore(gameHighScore);
+      }
+    } else if (ResCheckInput !== guess) {
+      if (gameTries <= 1) {
+        if (gameTries > 0) {
+          gameTries--;
+          countGameTries(gameTries);
+        } else {
+          messageBoxRemoveDisplay();
+          messageBoxContent(
+            `&#10060; you have to press Agian button at the top of the screen or Esc button on your keyBoard or ok button to restart game`
+          );
+        }
+        checkMessage = gameMessage(`&#128532; game over`);
+        if (gameHighScore > highScoreBoxValue) {
+          highScoreBoxValue = gameHighScore;
+          messageBoxContent(
+            `&#127882; congratulations, You have set a new record!`
+          );
+          document.removeEventListener("keydown", function () {
+            return true;
+          });
+          messageBoxRemoveDisplay();
+        } else {
+          highScoreBoxValue = highScoreBoxValue;
+        }
+        localStorage.setItem("currentHighScore", Number(highScoreBoxValue));
+        highScoreSaveBox(localStorage.getItem("currentHighScore"));
+        checkBtnDisable();
+        inputDisable(checkInput);
+        checkInput.value = "";
+      } else {
+        ResCheckInput > 20 || ResCheckInput < 1
+          ? (checkMessage = gameMessage(
+              `&#10060; the guessing number <br/> should be between (1 & 20)`
+            ))
+          : ResCheckInput > guess
+          ? (checkMessage = gameMessage(`&#128200; too high`))
+          : (checkMessage = gameMessage(`&#128201; too low`));
+        gameTries--;
+        countGameTries(gameTries);
       }
     }
-    // when guess is wrong
-    else if (guess !== secretNumber) {
-      ifEmpty = false;
-      if (score > 1) {
-        guess > secretNumber
-          ? overwriteMess('.message', '📈 Too High')
-          : overwriteMess('.message', '📉 Too Low');
-        decreaseScore();
+  }
+}
+
+function agian(checkInput) {
+  if (gameTries === 30 && checkMessage !== `&#127881; correct number!`) {
+    return true;
+  } else {
+    messageBoxAddDisplay();
+    guess = Math.trunc(Math.random() * 20) + 1;
+    if (gameTries == 0) {
+      gameTries = 30;
+      gameScore = 0;
+      gameHighScore = 0;
+      checkMessage = gameMessage(`&#128512; start guessing ... `);
+    } else {
+      gameTries = gameTries;
+      checkMessage = gameMessage(`&#129303; guess agian ... `);
+    }
+    countGameTries(gameTries);
+    countGameScore(gameScore);
+    countHighScore(gameHighScore);
+    changebackGroundColor(`background-color:#222`);
+    showSecretNumber("?");
+    secretNumberStyle(`width:11rem`);
+    checkInput.value = "";
+    checkBtnEnable();
+    inputEnable(checkInput);
+  }
+}
+
+agianBtn.addEventListener("click", () => {
+  console.log("yes1");
+  agian(checkInput());
+});
+
+checkBtn.addEventListener("click", () => {
+  check(checkInput());
+  emptyInput();
+});
+
+wrongIcon().addEventListener("click", messageBoxAddDisplay);
+
+messageBoxBtn1().addEventListener("click", () => {
+  messageBoxAddDisplay();
+  agian(checkInput());
+});
+
+messageBoxBtn2().addEventListener("click", messageBoxAddDisplay);
+
+document.addEventListener("keydown", (e) => {
+  if (messageBox().classList.contains("d-none") === false) {
+    if (e.key === "Enter") {
+      $(messageBoxBtn1()).click();
+    }
+  } else {
+    for (let i = 0; i < 10; i++) {
+      if (Number(e.key) === i) {
+        document.querySelector(".checkInput").focus();
+      }
+    }
+    if (e.key === "Enter") {
+      document.querySelector(".checkInput").blur();
+      check(checkInput());
+      emptyInput();
+    } else if (e.key === "Escape") {
+      agian(checkInput());
+    } else if (e.key === "Delete") {
+      if (gameTries < 30 || checkInputValue === guess) {
+        messageBoxRemoveDisplay();
+        messageBoxContent(
+          `&#10060; you can't reset the high score while playing`
+        );
       } else {
-        // overwriteMess() = document.querySelector(className).textContent = message;
-        // className = '.message'
-        // message = '💥 You Lost The Game!'
-        overwriteMess('.message', '💥 You Lost The Game!');
-        //document.querySelector('.score').textContent = score;
-        decreaseScore();
+        clearLocalStorage();
+        localStorageData();
       }
     }
   }
 });
 
-// keyboard clicks
-document.addEventListener('keydown', e => {
-  // when user press on any number of keyboard buttons
-  for (let i = 0; i < 10; i++) {
-    if (Number(e.key) === i) {
-      // remove value from input box to enter a new one
-      if (!ifEmpty) {
-        guessInput.value = '';
-        ifEmpty = true;
-      }
-        // guessInput = document.querySelector('.guess')
-        guessInput.focus();
-    }
-  }
-  // when user press enter from keyboard (it act like a mouse click event to press check button)
-  if (e.key === 'Enter') {
-    guessInput.blur();
-    checkBtn.click();
-  }
-  // when user press escape from keyboard (it act like a mouse click event to press again button)
-  else if (e.key === 'Escape') {
-    againBtn.click();
-  }
-  // when user delete any of input numbers
-  else if (e.key === 'Backspace') {
-    guessInput.focus();
-  }
+window.addEventListener("load", () => {
+  localStorageData();
+  countGameTries(gameTries);
+  countGameScore(gameScore);
+  countHighScore(gameHighScore);
 });
-// Decreasing score(tries) by each wrong answer
-function decreaseScore() {
-  score > 0 ? score-- : (score = 0);
-  document.querySelector('.score').textContent = score;
-}
-// message overwrite depending on the state
-function overwriteMess(className, message) {
-  document.querySelector(className).textContent = message;
-}
